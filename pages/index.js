beba16f5ae639fd8985a7ecc2a9e08dbe2675654
unknown <amincxo@gmail.com>
2025-02-18 18:46:07 +0330

    getStaticProps tamrin

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
 
-export default function Home() {
-  return (
+export default function Home({data}) {
+    return (
     <div>
-        <h3>amin tamrin</h3>
+        <h3>amin tamrin next </h3>
+        <h1>{data}</h1>
     </div>
   )
+}
+
+
+export async function getStaticProps () {
+    //fetch data from api
+    const data = 'Amin Borvayeh';
+
+    return {
+        props: {
+            data
+        },
+    };    
 }
