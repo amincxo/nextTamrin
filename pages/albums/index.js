63acda313cef08075fde3a721deb8107a9e3cbd1
unknown <amincxo@gmail.com>
2025-04-23 09:54:08 +0330

    context in getServerSideProps

---
+++ pages/albums/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Albumes({albums}) {
+    console.log(albums)
+  return (
+    <div>
+        <h1>Albumes</h1>
+        <ul>
+            {albums.map((album)=> (
+                <li key={album.id} >
+                    <h3>{album.title}</h3>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Albumes
+
+export async function getServerSideProps(context) {
+    const {parama , req , res } = context 
+    const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+    console.log(res)
+    const data = await response.json();
+    return { 
+        props: {albums : data},
+    };
+}
