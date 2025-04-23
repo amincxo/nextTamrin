0e31a12b4fddfc28298247697a9164576a3f4879
unknown <amincxo@gmail.com>
2025-04-23 14:34:42 +0330

    useEfect

---
+++ pages/todos/index.js
@@ -0,0 +1,17 @@
+import React, { useEffect ,useState } from 'react'
+
+function Todos() {
+    const [todos , setTodos] = useState ([]);
+    useEffect( ()=> {
+        fetch(`https://jsonplaceholder.typicode.com/todos`)
+        .then(res => res.json())
+        .then(data => setTodos(data)) ;
+    }, [] )
+  return (
+    <div>
+        {todos.length ? todos.map(todo => <h3 key={todo.id} >{todo.title}</h3>) : <h1>Loading...</h1> }
+    </div>
+  )
+}
+
+export default Todos
