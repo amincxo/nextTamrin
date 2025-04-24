74367648812657c6e6d0d868afb27febc1aca0d0
unknown <amincxo@gmail.com>
2025-04-24 07:33:04 +0330

    use SWR import and use and better

--- pages/todos/index.js
+++ pages/todos/index.js
@@ -1,15 +1,23 @@
-import React, { useEffect ,useState } from 'react'
+// import React, { useEffect ,useState } from 'react'
+import useSWR from "swr";
+const url = "https://jsonplaceholder.typicode.com/todos";
+const fecher = (url) => fetch(url).then((res) => res.json()) ;
 
 function Todos() {
-    const [todos , setTodos] = useState ([]);
-    useEffect( ()=> {
-        fetch(`https://jsonplaceholder.typicode.com/todos`)
-        .then(res => res.json())
-        .then(data => setTodos(data)) ;
-    }, [] )
+    // const [todos , setTodos] = useState ([]);
+    // useEffect( ()=> {
+    //     fetch(`https://jsonplaceholder.typicode.com/todos`)
+    //     .then(res => res.json())
+    //     .then(data => setTodos(data)) ;
+    // }, [] )
+
+    const { data , error } = useSWR( url, fecher );
+
+    console.log({data , error})
+    
   return (
     <div>
-        {todos.length ? todos.map(todo => <h3 key={todo.id} >{todo.title}</h3>) : <h1>Loading...</h1> }
+        {data ? data.map(todo => <h3 key={todo.id} >{todo.title}</h3>) : <h1>Loading...</h1> }
     </div>
   )
 }
